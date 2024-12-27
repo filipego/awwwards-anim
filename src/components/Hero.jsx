03e816f39bad3724a344032a2a76680150df5b10
Filipe Valente Gomes <filipego@gmail.com>
2024-12-27 18:34:14 -0500

    added text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -38,6 +38,41 @@ const Hero = () => {
                         />
                     </div>
                 </div>
+
+                <video
+                    ref={nextVideoRef}
+                    src={getVideoSrc(currentIndex)}
+                    loop
+                    muted
+                    id="next-video"
+                    className="absolute-center invisible absolute z-20 size-64 objct-cover object-center"
+                    onLoadedData={handleVideoload}
+                />
+
+                <video
+                    src={getVideoSrc(currentIndex === totalVideos - 1 ? 1 : currentIndex)}
+                    autoPlay
+                    loop
+                    muted
+                    className="absolute left-0 top-0 size-full object-cover object-center"
+                    onLoadedData={handleVideoload}
+                />
+            </div>
+            <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
+                G<b>A</b>MING
+            </h1>
+
+            <div className="absolute left-0 top-0 z-40 size-full">
+                <div className="mt-24 px-5 sm:px-10">
+                    <h1 className="special-font hero-heading text-blue-100">
+                        redefi<b>n</b>e
+                    </h1>
+                    <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
+                        Enter the Metagame Layer <br /> Unleash the Play Economy
+                    </p>
+
+
+                </div>
             </div>
         </div>
     )
